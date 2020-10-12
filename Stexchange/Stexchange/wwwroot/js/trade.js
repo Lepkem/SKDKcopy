f9676937db055fb82b9a1d3c1f08dc1498a1040c
Alexander <alexandervvugt@gmail.com>
2020-10-12 22:30:05 +0200

    Made carets beautifull

--- Stexchange/Stexchange/wwwroot/js/trade.js
+++ Stexchange/Stexchange/wwwroot/js/trade.js
@@ -3,6 +3,6 @@
 for (var i = 0; i < spanEl.length; i++) {
     spanEl[i].addEventListener("click", function () {
         this.parentElement.querySelector(".nested").classList.toggle("expanded");
-        this.classList.toggle("rotated90");
+        this.classList.toggle("rotated");
     });
 }
