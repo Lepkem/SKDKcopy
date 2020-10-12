d158ada8af3796e6a5a01108f4c878c1b83f45e1
Alexander <alexandervvugt@gmail.com>
2020-10-12 14:01:10 +0200

    Created trade page in mvc
    filter list styling is not working properly

---
+++ Stexchange/Stexchange/wwwroot/js/trade.js
@@ -0,0 +1,8 @@
+﻿var spanEl = document.getElementsByClassName("caret");
+
+for (var i = 0; i < spanEl.length; i++) {
+    spanEl[i].addEventListener("click", function () {
+        this.parentElement.querySelector(".nested").classList.toggle("expanded");
+        this.classList.toggle("rotated90");
+    });
+}
