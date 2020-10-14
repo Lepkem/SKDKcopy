178185e5e92ecb8c4ca97f2e0d36417ae53c2b83
Alexander <alexandervvugt@gmail.com>
2020-10-14 20:16:25 +0200

    made sort range inputs functional

--- Stexchange/Stexchange/wwwroot/js/trade.js
+++ Stexchange/Stexchange/wwwroot/js/trade.js
@@ -6,3 +6,23 @@ for (var i = 0; i < spanEl.length; i++) {
         this.classList.toggle("rotated");
     });
 }
+
+var sortRangeTogglers = document.getElementsByClassName("sort-range-toggle");
+var sortRange = document.getElementsByClassName("sort-range");
+var displays = document.getElementsByClassName("sort-range-display");
+
+for (var i = 0; i < sortRange.length; i++) {
+    sortRangeTogglers[i].addEventListener("click", togglerDecorator(i));
+    sortRange[i].addEventListener("input", (event) => {
+        let paragraph = document.getElementById(event.target.getAttribute("id") + "_amount");
+        let span = paragraph.childNodes[0];
+        span.innerText = event.target.value;
+    });
+}
+
+function togglerDecorator(j) {
+    return function () {
+        sortRange[j].disabled = !sortRange[j].disabled;
+        displays[j].hidden = !displays[j].hidden;
+    };
+}
