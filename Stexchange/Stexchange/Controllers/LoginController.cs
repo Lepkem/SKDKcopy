f2bb2e0005cce5dca6d3e63768528dfc29ef2f80
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2020-11-15 12:35:35 +0100

    Added user registration, added new message for invalid verificationcode, added docstrings to emailservice and logincontroller, added columns to user

--- Stexchange/Stexchange/Controllers/LoginController.cs
+++ Stexchange/Stexchange/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Stexchange.Data;
 using Stexchange.Data.Models;
@@ -8,7 +9,12 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Collections.Generic;
 
 namespace Stexchange.Controllers
 {
@@ -30,6 +36,11 @@ namespace Stexchange.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Compares verificationlink to user's verificationcode.
+        /// </summary>
+        /// <param name="id">Verificationcode of user</param>
+        /// <returns></returns>
         [HttpGet("[controller]/[action]/{id}")]
         public async Task<IActionResult> Verification(string id)
         {
@@ -40,22 +51,35 @@ namespace Stexchange.Controllers
 				return BadRequest();
 			}
 
+            var guids = (from code in Database.UserVerifications
+                         select code.Guid).ToArray();
+
+            // Checks if guid exists
+            if (!guids.Contains(guid))
+            {
+                TempData["Message"] = "Ongeldige Link";
+                return View("Verify");
+            }
+
             var verification = (from code in Database.UserVerifications
                                 where code.Guid == guid
                                 select code).FirstOrDefault();
 
-            //check if verificationlink has already been activated
-            if (verification.IsVerified == true)
+            var user = (from u in Database.Users
+                        where u.Verification.Guid == guid
+                        select u).FirstOrDefault();
+
+            // Checks if verificationlink has already been activated
+            if (user.IsVerified == true)
             {
                 TempData["Message"] = "E-mailverificatie Voltooid";
                 return View("Verify");
             }
-
             else
             {
                 if (!(verification is null))
                 {
-                    verification.IsVerified = true;
+                    user.IsVerified = true;
                     await Database.SaveChangesAsync();
 
                     TempData["Message"] = "E-mailverificatie Voltooid";
@@ -64,11 +88,15 @@ namespace Stexchange.Controllers
             }
         }
 
+        /// <summary>
+        /// Send new verificationlink to user if he isn't verified.
+        /// </summary>
+        /// <param name="vEmail">Emailaddress of user</param>
         [HttpGet("[controller]/[action]/{vEmail}")]
         public async Task<IActionResult> SendNewCode(string vEmail)
         {
             var user = (from u in Database.Users
-                        where !u.Verification.IsVerified
+                        where !u.IsVerified
                             && u.Email == vEmail
                         select u).FirstOrDefault();
             
@@ -93,7 +121,7 @@ https://{ControllerContext.HttpContext.Request.Host}/login/Verification/{user.Ve
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register(string email)
+        public async Task<IActionResult> Register(string email, string vEmail, string password, string confirm_password, string username, string postalcode)
         {
             try
             {
@@ -101,12 +129,42 @@ https://{ControllerContext.HttpContext.Request.Host}/login/Verification/{user.Ve
                 {
                     if (!new EmailAddressAttribute().IsValid(email))
                     {
-                        TempData["Message"] = "E-mail is ongeldig";
+                        TempData["Message"] = "InvalidEmail";
+                        return View("Login");
+                    }
+
+                    if (email != vEmail)
+                    {
+                        TempData["Message"] = "IncorrectEmails";
                         return View("Login");
                     }
                     
-                    //Generates verification code
-                    Random rnd = new Random();
+                    // Checks if postal code is valid
+                    if (!new Regex(@"\d{4}[A-Z]{2}", RegexOptions.IgnoreCase).IsMatch(postalcode))
+                    {
+                        TempData["Message"] = "InvalidPostalCode";
+                        return View("Login");
+                    }
+
+                    if (password != confirm_password)
+                    {
+                        TempData["Message"] = "IncorrectPasswords";
+                        return View("Login");
+                    }
+
+                    // Checks if email already exists in database
+                    if (Database.Users.Any(u => u.Email == email))
+                    {
+                        TempData["Message"] = "EmailExists";
+                        return View("Login");
+                    }
+
+                    // Checks if username already exists in database
+                    if (Database.Users.Any(u => u.Username == username))
+                    {
+                        TempData["Message"] = "UsernameTaken";
+                        return View("Login");
+                    }
 
                     // Create a new UserVerification object with a new unique Guid and verification code
                     var verification = new UserVerification()
@@ -117,15 +175,12 @@ https://{ControllerContext.HttpContext.Request.Host}/login/Verification/{user.Ve
                     var new_User = new User()
                     {
                         Email = email,
+                        Username = username,
+                        Postal_Code = postalcode.ToUpper(),
+                        Password = CreatePasswordHash(password, username),
+                        Created_At = DateTime.Now,
                         Verification = verification
                     };
-
-                    if (Database.Users.Any(u => u.Email == email))
-                    {
-                        // found users with the given email
-                        TempData["Message"] = "E-mail is al gebruikt";
-                        return View("Login");
-                    }
                  
                     await Database.AddAsync(new_User);
                     await Database.SaveChangesAsync();
@@ -150,10 +205,29 @@ https://{ControllerContext.HttpContext.Request.Host}/login/Verification/{verific
             return View("Login");
         }
 
-        private void SendEmail(string address, string body)
+        /// <summary>
+        /// Adds message to queue
+        /// </summary>
+        /// <param name="address">The mail address of the user</param>
+        /// <param name="body">The mail message</param>
+        private void SendEmail(string address, string body) => EmailService.QueueMessage(address, body);
+
+        /// <summary>
+        /// Given a password and salt, returns a salted SHA512 hash.
+        /// </summary>
+        /// <param name="password">The password</param>
+        /// <param name="salt">The salt to use (username)</param>
+        /// <returns>The new password hash</returns>
+        private byte[] CreatePasswordHash(string password, string salt)
         {
-			EmailService.QueueMessage(address, body);
-		}
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentException($"'{nameof(password)}' cannot be null or empty");
+            if (string.IsNullOrEmpty(salt))
+                throw new ArgumentException($"'{nameof(salt)}' cannot be null or empty");        
+            
+            using var sha512Hash = SHA512.Create();
+            return sha512Hash.ComputeHash(Encoding.UTF8.GetBytes($"{salt}#:#{password}"));
+        }
 
         public IActionResult Error()
         {
---
+++ Stexchange/Stexchange/Migrations/20201114131839_AddedIsVerified.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stexchange.Migrations
+{
+    public partial class AddedIsVerified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Users");
+        }
+    }
+}
