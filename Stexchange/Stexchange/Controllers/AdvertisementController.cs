d1f89f06bd7a19f9d09aea1f8c4b0687ddb7f809
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2020-11-15 15:46:34 +0100

    Added PostAdvertisement page

---
+++ Stexchange/Stexchange/Controllers/AdvertisementController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stexchange.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        public IActionResult PostAdvertisement()
+        {
+            return View("PostAdvertisement");
+        }
+    }
+}
