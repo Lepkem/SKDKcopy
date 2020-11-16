f78e931b459052e008bbb0c27643c9f4421949b7
Alexander <alexandervvugt@gmail.com>
2020-11-16 12:57:27 +0100

    Created field and method stub

--- Stexchange/Stexchange/Models/TradeViewModel.cs
+++ Stexchange/Stexchange/Models/TradeViewModel.cs
@@ -7,7 +7,25 @@ namespace Stexchange.Models
 {
     public class TradeViewModel
     {
-        public List<Object> RetrieveListings(string token)
+        private static List<Object> listingCache;
+
+        private static void renewListingCache(ref List<Object> cache)
+        {
+            throw new NotImplementedException();
+            /*
+            var queryresult = 
+            cache = queryresult
+            * Query:
+            * select A.*, I.image, F.with_pot, F.water, F.light, F.ph, F.neutrients, F.indigenous, F.type, F.give_away
+            * from Advertisement as A
+            * join Image as I on A.id=I.ad_id
+            * join Filter as F on A.id=F.id
+            * where A.invisible=false
+            * order by A.created_at desc;
+            */
+        }
+
+        public List<Object> RetrieveListings(long token)
         {
             throw new NotImplementedException();
         }
