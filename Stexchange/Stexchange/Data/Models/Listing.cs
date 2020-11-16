3ebd3355f4ae441bf030abc89af1f579a2352f17
Alexander <alexandervvugt@gmail.com>
2020-11-16 18:49:25 +0100

    Created Listing table

---
+++ Stexchange/Stexchange/Data/Models/Listing.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stexchange.Data.Models
+{
+    public class Listing
+    {
+        [NotMapped]
+        private string zipCode;
+
+        [Column("id", TypeName = "serial"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("title", TypeName = "varchar(80)")]
+        public string Title { get; set; }
+        [Column("description", TypeName = "text")]
+        public string Description { get; set; }
+        [Column("name_nl", TypeName = "varchar(30)")]
+        public string NameNl { get; set; }
+        [Column("name_lt", TypeName = "varchar(30)")]
+        public string NameLatin { get; set; }
+        [Column("quantity", TypeName = "int"), Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+        [NotMapped]
+        public List<Object> Pictures { get; set; }
+        [Column("user_id", TypeName = "int")]
+        public int UserId { get; set; }
+        public User Owner { get; set; }
+        [NotMapped]
+        public string OwningUserName { get; set; }
+        [NotMapped]
+        public List<string> Categories { get; set; }
+        [Column("created_at", TypeName = "timestamp"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [NotMapped]
+        public Object Rating { get; set; }
+        [NotMapped]
+        public double Distance { get; set; }
+        [Column("visible", TypeName = "tinyint(1)")]
+        public bool Visible { get; set; }
+        [Column("renewed", TypeName = "tinyint(1)")]
+        public bool Renewed { get; set; }
+
+        /// <summary>
+        /// Calculate the distance between the users zipCode and the posters zipCode.
+        /// </summary>
+        /// <param name="myZipCode">The users zipCode</param>
+        public void SetDistance(string myZipCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
