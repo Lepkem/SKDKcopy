b053a7b81e70613fb1020d0977ed7de2d871bbbf
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2020-11-18 23:05:44 +0100

    Added image input + field, user can now insert multiple images, fixed description field

--- Stexchange/Stexchange/wwwroot/js/postadvertisement.js
+++ Stexchange/Stexchange/wwwroot/js/postadvertisement.js
@@ -1,22 +1,79 @@
 ﻿var acc = document.getElementsByClassName("accordion");
 var filter = document.getElementById("extrafilters");
-filter.style.display = "none";
-var i;
 
+/*hides extrafilter field*/
+filter.style.display = "none";
+
+/*click function for hiding/showing panel*/
 acc[0].addEventListener("click", function () {
-    /* Toggle between hiding and showing the panel */
     var panel = this.nextElementSibling;
+
     if (panel.style.display === "block") {
         panel.style.display = "none";
     } else {
         panel.style.display = "block";
     }
 });
+/*message*/
+var message = document.getElementById("messagepicture");
 
+/*previous button*/
+var previous = document.getElementsByClassName("previous");
+
+/*next button*/
+var next = document.getElementsByClassName("next");
+previous[0].style.display = "none";
+next[0].style.display = "none";
+
+/*image field*/
+var image = document.getElementById("imgoutput");
+
+/*image files*/
+var files = document.getElementById("imginput").files;
+
+/*span text*/
+var imagecounter = document.getElementById("imagecounter")
+
+var currentImage = 0;
+
+/*inserts first image in list into img field*/
 var loadFile = function (event) {
-    var image = document.getElementById("imgoutput");
-    image.src = URL.createObjectURL(event.target.files[0]);
+    currentImage = 0;
+    image.src = URL.createObjectURL(files[currentImage]);
+    imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    message.style.display = "none";
+    previous[0].style.display = "inline";
+    next[0].style.display = "inline";
 };
+
+/*inserts next file into img field*/
+function nextImg(n) {
+    if (currentImage == files.length - 1) {
+        currentImage = 0;
+        image.src = URL.createObjectURL(files[currentImage]);
+        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    }
+    else {
+        currentImage += n;
+        image.src = URL.createObjectURL(files[currentImage]);
+        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    }
+}
+
+/*inserts previous file into img field*/
+function previousImg(n) {
+    if (currentImage > 0) {
+        currentImage -= n;
+        image.src = URL.createObjectURL(files[currentImage]);
+        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    } else {
+        currentImage = files.length - 1;
+        image.src = URL.createObjectURL(files[currentImage]);
+        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    }
+}
+
+
 
 
 
