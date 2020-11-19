798d83b017a9ec8521d5db5dc905bd9301162bda
Alexander <alexandervvugt@gmail.com>
2020-11-19 15:57:23 +0100

    [cherry-pick] lastormodified column added to Listings

--- Stexchange/Stexchange/Data/Models/Listing.cs
+++ Stexchange/Stexchange/Data/Models/Listing.cs
@@ -12,37 +12,55 @@ namespace Stexchange.Data.Models
         [NotMapped]
         private string zipCode;
 
+        [NotMapped]
+        private double distance = -1;
+
         [Column("id", TypeName = "serial"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Column("title", TypeName = "varchar(80)")]
         public string Title { get; set; }
+
         [Column("description", TypeName = "text")]
         public string Description { get; set; }
+
         [Column("name_nl", TypeName = "varchar(30)")]
         public string NameNl { get; set; }
+
         [Column("name_lt", TypeName = "varchar(30)")]
         public string NameLatin { get; set; }
+
         [Column("quantity", TypeName = "int"), Range(1, int.MaxValue)]
         public int Quantity { get; set; }
+
         [NotMapped]
         public List<ImageData> Pictures { get; set; }
+
         [Column("user_id", TypeName = "bigint(20) unsigned")]
         public int UserId { get; set; }
+
         public User Owner { get; set; }
+
         [NotMapped]
         public string OwningUserName { get; set; }
+
         [NotMapped]
-        public List<FilterListing> Categories { get; set; }
+        public List<string> Categories { get; set; }
+
         [Column("created_at", TypeName = "timestamp"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedAt { get; set; }
+
         [NotMapped]
-        public Object Rating { get; set; }
-        [NotMapped]
-        public double Distance { get; set; }
+        public double Distance { get => distance; set => distance = value; }
+
         [Column("visible", TypeName = "tinyint(1)")]
         public bool Visible { get; set; }
+
         [Column("renewed", TypeName = "tinyint(1)")]
         public bool Renewed { get; set; }
+
+        [Column("last_modified", TypeName = "timestamp"), DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastModified { get; set; }
 
         /// <summary>
         /// Calculate the distance between the users zipCode and the posters zipCode.
