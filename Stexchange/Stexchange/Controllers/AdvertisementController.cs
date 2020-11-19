939bb8b466c5ecb70e9a50c8600b4a9f28dc7ebc
lepkem <jordamn.g@gmail.com>
2020-11-19 13:50:48 +0100

    micro progress on listing backend

--- Stexchange/Stexchange/Controllers/AdvertisementController.cs
+++ Stexchange/Stexchange/Controllers/AdvertisementController.cs
@@ -3,11 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Stexchange.Data;
 
 namespace Stexchange.Controllers
 {
     public class AdvertisementController : Controller
     {
+        public AdvertisementController(Database db, IConfiguration config)
+        {
+            Database _databse = db;
+            IConfiguration _config = config;
+
+
+        }
+
+        private Database _database { get; }
+        private IConfiguration _config { get; }
+
+
+        [HttpPost]
         public IActionResult PostAdvertisement()
         {
             return View();
---
+++ Stexchange/Stexchange/Data/Builders/ListingBuilder.cs
@@ -0,0 +1,82 @@
+﻿using Stexchange.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Stexchange.Data.Builders
+{
+    public class ListingBuilder
+    {
+        private Listing construct;
+
+        /// <summary>
+        /// Initializes a ListingBuilder with a clean construct.
+        /// </summary>
+        public ListingBuilder()
+        {
+            Reset();
+        }
+
+<<<<<<< HEAD
+=======
+        /// <summary>
+        /// Initializes a ListingBuilder with an existing construct.
+        /// </summary>
+        /// <param name="existing"></param>
+        public ListingBuilder(Listing existing)
+        {
+            construct = existing;
+        }
+
+        /// <summary>
+        /// Resets this ListingBuilder to a clean construct.
+        /// </summary>
+>>>>>>> 38ca5e2... Documented ListingBuilder, removed redundant null check
+        public void Reset()
+        {
+            construct = new Listing();
+        }
+
+        /// <summary>
+        /// Returns the construct of this ListingBuilder.
+        /// </summary>
+        /// <returns></returns>
+        public Listing Complete() => construct;
+
+        /// <summary>
+        /// Sets the specified property to the specified value.
+        /// </summary>
+        /// <param name="propertyInfo">PropertyInfo Object representing the property to set</param>
+        /// <param name="value">The value for the property</param>
+        /// <returns></returns>
+        public ListingBuilder SetProperty(PropertyInfo propertyInfo, object value)
+        {
+            if(value.GetType().Equals(propertyInfo.PropertyType))
+            {
+                propertyInfo.SetValue(this, value);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the specified property to the specified value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to set</param>
+        /// <param name="value">The value for the property</param>
+        /// <returns></returns>
+        public ListingBuilder SetProperty(string propertyName, object value)
+        {
+            if(propertyName is object)
+            {
+                var propertyInfo = typeof(Listing).GetProperty(propertyName);
+                if (propertyInfo is object)
+                {
+                    return SetProperty(propertyInfo, value);
+                }
+            }
+            return this;
+        }
+    }
+}
