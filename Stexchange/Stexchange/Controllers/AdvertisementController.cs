786f8223fa23c6d468992c66f1e41d599ef97a58
lepkem <jordamn.g@gmail.com>
2020-11-19 18:33:41 +0100

    corrected PostAdvertisement and params ad controller

--- Stexchange/Stexchange/Controllers/AdvertisementController.cs
+++ Stexchange/Stexchange/Controllers/AdvertisementController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Stexchange.Data;
+using Stexchange.Data.Models;
 
 namespace Stexchange.Controllers
 {
@@ -21,11 +22,20 @@ namespace Stexchange.Controllers
         private Database _database { get; }
         private IConfiguration _config { get; }
 
+        
 
         [HttpPost]
-        public IActionResult PostAdvertisement()
+        public IActionResult PostAdvertisement(ImageData img, string title, string description, 
+            string name_nl, int quantity, string plant_type, string name_lt="", string light="", 
+            string water="", string ph="", string indigenous="", string with_pot="", string give_away="")
         {
-            return View();
+            //TODO: put in all name values empty or not
+            //TODO: validate all name values from the form per value if they are empty or wrong
+            //TODO: build a listing using the listing builder class 
+            //TODO: use the category list 
+
+
+            return View(); // todo: insert something here
         }
     }
 }
