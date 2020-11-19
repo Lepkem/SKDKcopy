38ca5e22ca9ee6ef3b49c84fcf611bbf53696b38
Alexander <alexandervvugt@gmail.com>
2020-11-19 13:45:30 +0100

    Documented ListingBuilder, removed redundant null check

--- Stexchange/Stexchange/Data/Builders/ListingBuilder.cs
+++ Stexchange/Stexchange/Data/Builders/ListingBuilder.cs
@@ -11,32 +11,58 @@ namespace Stexchange.Data.Builders
     {
         private Listing construct;
 
+        /// <summary>
+        /// Initializes a ListingBuilder with a clean construct.
+        /// </summary>
         public ListingBuilder()
         {
             Reset();
         }
 
+        /// <summary>
+        /// Initializes a ListingBuilder with an existing construct.
+        /// </summary>
+        /// <param name="existing"></param>
         public ListingBuilder(Listing existing)
         {
             construct = existing;
         }
 
+        /// <summary>
+        /// Resets this ListingBuilder to a clean construct.
+        /// </summary>
         public void Reset()
         {
             construct = new Listing();
         }
 
+        /// <summary>
+        /// Returns the construct of this ListingBuilder.
+        /// </summary>
+        /// <returns></returns>
         public Listing Complete() => construct;
 
+        /// <summary>
+        /// Sets the specified property to the specified value.
+        /// </summary>
+        /// <param name="propertyInfo">PropertyInfo Object representing the property to set</param>
+        /// <param name="value">The value for the property</param>
+        /// <returns></returns>
         public ListingBuilder SetProperty(PropertyInfo propertyInfo, object value)
         {
-            if(value is object && value.GetType().Equals(propertyInfo.PropertyType))
+            if(value.GetType().Equals(propertyInfo.PropertyType))
             {
                 propertyInfo.SetValue(this, value);
             }
             return this;
         }
 
+        /// <summary>
+        /// Sets the specified property to the specified value.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to set</param>
+        /// <param name="value">The value for the property</param>
+        /// <returns></returns>
         public ListingBuilder SetProperty(string propertyName, object value)
         {
             if(propertyName is object)
