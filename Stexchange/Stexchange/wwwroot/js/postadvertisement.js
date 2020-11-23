84298c24ac1b063f2aaf0f5c5c8a750b923fc2a8
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2020-11-23 16:07:12 +0100

    Fixed image not displaying in chrome,IE,Edge

--- Stexchange/Stexchange/wwwroot/js/postadvertisement.js
+++ Stexchange/Stexchange/wwwroot/js/postadvertisement.js
@@ -1,10 +1,10 @@
 ﻿var acc = document.getElementsByClassName("accordion");
 var filter = document.getElementById("extrafilters");
 
-/*hides extrafilter field*/
+/* hides extrafilter field*/
 filter.style.display = "none";
 
-/*click function for hiding/showing panel*/
+/* click function for hiding/showing panel*/
 acc[0].addEventListener("click", function () {
     var panel = this.nextElementSibling;
 
@@ -14,63 +14,80 @@ acc[0].addEventListener("click", function () {
         panel.style.display = "block";
     }
 });
-/*message*/
+/* message*/
 var message = document.getElementById("messagepicture");
 
-/*previous button*/
+/* previous button*/
 var previous = document.getElementsByClassName("previous");
 
-/*next button*/
+/* next button*/
 var next = document.getElementsByClassName("next");
 previous[0].style.display = "none";
 next[0].style.display = "none";
 
-/*image field*/
+/* image field*/
 var image = document.getElementById("imgoutput");
 
-/*image files*/
+/* image files*/
 var files = document.getElementById("imginput").files;
 
-/*span text*/
+/* span text*/
 var imagecounter = document.getElementById("imagecounter")
 
 var currentImage = 0;
 
-/*inserts first image in list into img field*/
+/* inserts first image in list into img field*/
 var loadFile = function (event) {
-    currentImage = 0;
-    image.src = URL.createObjectURL(files[currentImage]);
-    imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    filereader(0); /*display first image*/
+    imagecount();
     message.style.display = "none";
     previous[0].style.display = "inline";
     next[0].style.display = "inline";
 };
 
-/*inserts next file into img field*/
+/* inserts next file into img field*/
 function nextImg(n) {
+    var files = document.getElementById("imginput").files;
     if (currentImage == files.length - 1) {
         currentImage = 0;
-        image.src = URL.createObjectURL(files[currentImage]);
-        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
     }
     else {
         currentImage += n;
-        image.src = URL.createObjectURL(files[currentImage]);
-        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
+    }
+    filereader(currentImage);
+    imagecount();
+}
+
+
+/* inserts previous file into img field*/
+function previousImg(n) {
+    var files = document.getElementById("imginput").files;
+    if (currentImage > 0) {
+        currentImage -= n;
+    } else {
+        currentImage = files.length - 1;
+    }
+    filereader(currentImage);
+    imagecount();
+}
+
+/*reads file as a data url*/
+function filereader(n) {
+    var files = document.getElementById("imginput").files[n];
+    if (files) {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (event) {
+            image.src = fileReader.result;
+        };
+        fileReader.readAsDataURL(files);
     }
 }
 
-/*inserts previous file into img field*/
-function previousImg(n) {
-    if (currentImage > 0) {
-        currentImage -= n;
-        image.src = URL.createObjectURL(files[currentImage]);
-        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
-    } else {
-        currentImage = files.length - 1;
-        image.src = URL.createObjectURL(files[currentImage]);
-        imagecounter.textContent = (currentImage + 1) + "/" + files.length;
-    }
+/*image counter*/
+function imagecount() {
+    var files = document.getElementById("imginput").files;
+    imagecounter.textContent = (currentImage + 1) + "/" + files.length;
 }
 
 
