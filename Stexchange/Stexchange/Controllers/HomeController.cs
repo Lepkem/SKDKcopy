639f1be310d9d88db38d07e73c2dec7b94f4d820
esmaiel1 <60741207+esmaiel1@users.noreply.github.com>
2020-11-24 16:40:15 +0100

    LOGNNNN MODIFIEDD

--- Stexchange/Stexchange/Controllers/HomeController.cs
+++ Stexchange/Stexchange/Controllers/HomeController.cs
@@ -38,10 +38,6 @@ namespace Stexchange.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
