91866c683c5f026348467fae7ad938ee598aede0
Jordan Guerrero <jordanova@Jordans-MacBook-Air.local>
2020-11-26 14:33:19 +0100

    wrote listing validation

--- Stexchange/Stexchange/Data/Validation/ListingValidator.cs
+++ Stexchange/Stexchange/Data/Validation/ListingValidator.cs
@@ -12,8 +12,19 @@ namespace Stexchange.Data.Validation
         public ListingValidator()
         {
             RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Title.Length <=80);
+
+            RuleFor(x => x.Quantity).NotNull();
+            RuleFor(x => x.Quantity).GreaterThan(0);
+
+            RuleFor(x => x.NameLatin.Length < 50);
+
             RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Description.Length <= 500);
+
             RuleFor(x => x.NameNl).NotEmpty();
+            RuleFor(x => x.NameNl.Length <= 50);
+            
             RuleFor(x => x.Quantity).GreaterThan(0);
 
 
