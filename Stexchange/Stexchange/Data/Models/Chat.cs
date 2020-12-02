96748c639c165e14ac620f3dd34c8c17ae46b210
Alexander <alexandervvugt@gmail.com>
2020-12-02 19:13:24 +0100

    feat(chat) created Chat and Message tables

---
+++ Stexchange/Stexchange/Data/Models/Chat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stexchange.Data.Models
+{
+    public class Chat
+    {
+        [Column("id", TypeName = "serial"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("ad_id", TypeName = "bigint(20) unsigned")]
+        public int AdId { get; set; }
+
+        [Column("responder_id", TypeName = "bigint(20) unsigned")]
+        public int ResponderId { get; set; }
+
+        [NotMapped]
+        public User Responder { get; set; }
+
+        [NotMapped]
+        public User Poster { get; set; }
+
+        [NotMapped]
+        public Listing Listing { get; set; }
+
+        [NotMapped]
+        public List<Message> Messages { get; set; }
+    }
+}
--- Stexchange/Stexchange/Migrations/20201202180944_chat_alternate_key.Designer.cs
+++ Stexchange/Stexchange/Migrations/20201202180944_chat_alternate_key.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stexchange.Data;
 
 namespace Stexchange.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20201202180944_chat_alternate_key")]
+    partial class chat_alternate_key
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
