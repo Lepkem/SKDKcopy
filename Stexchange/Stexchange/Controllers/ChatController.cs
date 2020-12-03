4956eab35debaf46e689271cca8d835dc1bf0ea4
Alexander <alexandervvugt@gmail.com>
2020-12-03 19:42:04 +0100

    feat(Chat): created Model and Controller, and method to retrieve chats

--- Stexchange/Stexchange/Controllers/ChatController.cs
+++ Stexchange/Stexchange/Controllers/ChatController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Stexchange.Data;
+using Stexchange.Models;
 
 namespace Stexchange.Controllers
 {
     public class ChatController : Controller
     {
+        private Database _db;
+
+        public ChatController(Database db)
+        {
+            _db = db;
+        }
+
         public IActionResult Chat()
         {
-            return View();
+            return View(model: new ChatViewModel(_db));
         }
     }
 }
